eed4d9c1b83d4d49b5da2cfb4369a0c2b406745d
nikolay <jetrok@yandex.ru>
2015-07-29 18:58:54 +0300

    lool

--- app/map.js
+++ app/map.js
@@ -12,12 +12,31 @@ MapManager.prototype.init = function() {
 };
 
 MapManager.prototype.addMarker = function(latlng) {
+    var map = this.map;
     var marker = L.marker(latlng, {
         draggable: true,
         clickable: true
-    }).addTo(this.map);
+    }).addTo(map);
+
+    function changeItemPos(element, points) {
+        element.css({
+            'top': points.y - element.outerHeight() - 40 + 'px',
+            'left': points.x - element.width() / 2 + 'px'
+        }).show();
+    }
+
+    marker.on('click', function(e) {
+        var element = $('.chat-panel');
+        var points = map.latLngToLayerPoint(e.latlng);
+        changeItemPos(element, points);
+
+        map.on('zoomend drag', function() {
+            console.log(e.latlng)
+            points = map.latLngToLayerPoint(e.latlng);
+            console.log(points);
+            changeItemPos(element, points);
+        });
+    });
+
     this.markers.push(marker);
-
-    console.log(marker)
 };
-
